fb28b94dc567237834ad3704d6983d1d42833052
KamoKamo <tamutamuphone@gmail.com>
2019-04-26 17:54:17 +0900

    iroiro

---
+++ ProroboQR/MainForm.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace ProroboQR
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardPictureBox0 = new System.Windows.Forms.PictureBox();
+            this.cardsGroupBox = new System.Windows.Forms.GroupBox();
+            this.cardPictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cardPictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cardPictureBox3 = new System.Windows.Forms.PictureBox();
+            this.cardPictureBox4 = new System.Windows.Forms.PictureBox();
+            this.cardPictureBox5 = new System.Windows.Forms.PictureBox();
+            this.cardPictureBox6 = new System.Windows.Forms.PictureBox();
+            this.cardPictureBox7 = new System.Windows.Forms.PictureBox();
+            this.sysinfoGroupBox = new System.Windows.Forms.GroupBox();
+            this.programTextBox = new System.Windows.Forms.TextBox();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.isReConCehckBox = new System.Windows.Forms.CheckBox();
+            this.twistTimeNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.moveTimeNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox0)).BeginInit();
+            this.cardsGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox7)).BeginInit();
+            this.sysinfoGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.twistTimeNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moveTimeNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardPictureBox0
+            // 
+            this.cardPictureBox0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPictureBox0.Location = new System.Drawing.Point(11, 100);
+            this.cardPictureBox0.Name = "cardPictureBox0";
+            this.cardPictureBox0.Size = new System.Drawing.Size(100, 100);
+            this.cardPictureBox0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardPictureBox0.TabIndex = 0;
+            this.cardPictureBox0.TabStop = false;
+            this.cardPictureBox0.Tag = "0";
+            // 
+            // cardsGroupBox
+            // 
+            this.cardsGroupBox.Controls.Add(this.cardPictureBox7);
+            this.cardsGroupBox.Controls.Add(this.cardPictureBox6);
+            this.cardsGroupBox.Controls.Add(this.cardPictureBox5);
+            this.cardsGroupBox.Controls.Add(this.cardPictureBox4);
+            this.cardsGroupBox.Controls.Add(this.cardPictureBox3);
+            this.cardsGroupBox.Controls.Add(this.cardPictureBox2);
+            this.cardsGroupBox.Controls.Add(this.cardPictureBox1);
+            this.cardsGroupBox.Controls.Add(this.cardPictureBox0);
+            this.cardsGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.cardsGroupBox.Name = "cardsGroupBox";
+            this.cardsGroupBox.Size = new System.Drawing.Size(960, 320);
+            this.cardsGroupBox.TabIndex = 1;
+            this.cardsGroupBox.TabStop = false;
+            this.cardsGroupBox.Text = "プログラム";
+            // 
+            // cardPictureBox1
+            // 
+            this.cardPictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPictureBox1.Location = new System.Drawing.Point(133, 100);
+            this.cardPictureBox1.Name = "cardPictureBox1";
+            this.cardPictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.cardPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardPictureBox1.TabIndex = 0;
+            this.cardPictureBox1.TabStop = false;
+            this.cardPictureBox1.Tag = "1";
+            // 
+            // cardPictureBox2
+            // 
+            this.cardPictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPictureBox2.Location = new System.Drawing.Point(256, 100);
+            this.cardPictureBox2.Name = "cardPictureBox2";
+            this.cardPictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.cardPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardPictureBox2.TabIndex = 0;
+            this.cardPictureBox2.TabStop = false;
+            this.cardPictureBox2.Tag = "2";
+            // 
+            // cardPictureBox3
+            // 
+            this.cardPictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPictureBox3.Location = new System.Drawing.Point(378, 100);
+            this.cardPictureBox3.Name = "cardPictureBox3";
+            this.cardPictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.cardPictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardPictureBox3.TabIndex = 0;
+            this.cardPictureBox3.TabStop = false;
+            this.cardPictureBox3.Tag = "3";
+            // 
+            // cardPictureBox4
+            // 
+            this.cardPictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPictureBox4.Location = new System.Drawing.Point(502, 100);
+            this.cardPictureBox4.Name = "cardPictureBox4";
+            this.cardPictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.cardPictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardPictureBox4.TabIndex = 0;
+            this.cardPictureBox4.TabStop = false;
+            this.cardPictureBox4.Tag = "4";
+            // 
+            // cardPictureBox5
+            // 
+            this.cardPictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPictureBox5.Location = new System.Drawing.Point(622, 100);
+            this.cardPictureBox5.Name = "cardPictureBox5";
+            this.cardPictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.cardPictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardPictureBox5.TabIndex = 0;
+            this.cardPictureBox5.TabStop = false;
+            this.cardPictureBox5.Tag = "5";
+            // 
+            // cardPictureBox6
+            // 
+            this.cardPictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPictureBox6.Location = new System.Drawing.Point(739, 100);
+            this.cardPictureBox6.Name = "cardPictureBox6";
+            this.cardPictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.cardPictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardPictureBox6.TabIndex = 0;
+            this.cardPictureBox6.TabStop = false;
+            this.cardPictureBox6.Tag = "6";
+            // 
+            // cardPictureBox7
+            // 
+            this.cardPictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPictureBox7.Location = new System.Drawing.Point(851, 100);
+            this.cardPictureBox7.Name = "cardPictureBox7";
+            this.cardPictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.cardPictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardPictureBox7.TabIndex = 0;
+            this.cardPictureBox7.TabStop = false;
+            this.cardPictureBox7.Tag = "7";
+            // 
+            // sysinfoGroupBox
+            // 
+            this.sysinfoGroupBox.Controls.Add(this.label2);
+            this.sysinfoGroupBox.Controls.Add(this.label1);
+            this.sysinfoGroupBox.Controls.Add(this.moveTimeNumericUpDown);
+            this.sysinfoGroupBox.Controls.Add(this.twistTimeNumericUpDown);
+            this.sysinfoGroupBox.Controls.Add(this.isReConCehckBox);
+            this.sysinfoGroupBox.Controls.Add(this.logTextBox);
+            this.sysinfoGroupBox.Controls.Add(this.programTextBox);
+            this.sysinfoGroupBox.Location = new System.Drawing.Point(12, 338);
+            this.sysinfoGroupBox.Name = "sysinfoGroupBox";
+            this.sysinfoGroupBox.Size = new System.Drawing.Size(960, 286);
+            this.sysinfoGroupBox.TabIndex = 2;
+            this.sysinfoGroupBox.TabStop = false;
+            this.sysinfoGroupBox.Text = "システム情報";
+            // 
+            // programTextBox
+            // 
+            this.programTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.programTextBox.Location = new System.Drawing.Point(11, 20);
+            this.programTextBox.Multiline = true;
+            this.programTextBox.Name = "programTextBox";
+            this.programTextBox.Size = new System.Drawing.Size(302, 253);
+            this.programTextBox.TabIndex = 0;
+            this.programTextBox.TextChanged += new System.EventHandler(this.programTextBox_TextChanged);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(324, 20);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(627, 205);
+            this.logTextBox.TabIndex = 1;
+            // 
+            // isReConCehckBox
+            // 
+            this.isReConCehckBox.AutoSize = true;
+            this.isReConCehckBox.Location = new System.Drawing.Point(340, 243);
+            this.isReConCehckBox.Name = "isReConCehckBox";
+            this.isReConCehckBox.Size = new System.Drawing.Size(163, 16);
+            this.isReConCehckBox.TabIndex = 2;
+            this.isReConCehckBox.Text = "自動で再接続を試行します。";
+            this.isReConCehckBox.UseVisualStyleBackColor = true;
+            // 
+            // twistTimeNumericUpDown
+            // 
+            this.twistTimeNumericUpDown.Location = new System.Drawing.Point(814, 241);
+            this.twistTimeNumericUpDown.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.twistTimeNumericUpDown.Name = "twistTimeNumericUpDown";
+            this.twistTimeNumericUpDown.Size = new System.Drawing.Size(60, 19);
+            this.twistTimeNumericUpDown.TabIndex = 3;
+            this.twistTimeNumericUpDown.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // moveTimeNumericUpDown
+            // 
+            this.moveTimeNumericUpDown.Location = new System.Drawing.Point(622, 241);
+            this.moveTimeNumericUpDown.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.moveTimeNumericUpDown.Name = "moveTimeNumericUpDown";
+            this.moveTimeNumericUpDown.Size = new System.Drawing.Size(60, 19);
+            this.moveTimeNumericUpDown.TabIndex = 3;
+            this.moveTimeNumericUpDown.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(539, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "前後移動時間";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(719, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ツイスト移動時間";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.sysinfoGroupBox);
+            this.Controls.Add(this.cardsGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "ProroboQR";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox0)).EndInit();
+            this.cardsGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox7)).EndInit();
+            this.sysinfoGroupBox.ResumeLayout(false);
+            this.sysinfoGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.twistTimeNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moveTimeNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardPictureBox0;
+        private System.Windows.Forms.GroupBox cardsGroupBox;
+        private System.Windows.Forms.GroupBox sysinfoGroupBox;
+        private System.Windows.Forms.PictureBox cardPictureBox7;
+        private System.Windows.Forms.PictureBox cardPictureBox6;
+        private System.Windows.Forms.PictureBox cardPictureBox5;
+        private System.Windows.Forms.PictureBox cardPictureBox4;
+        private System.Windows.Forms.PictureBox cardPictureBox3;
+        private System.Windows.Forms.PictureBox cardPictureBox2;
+        private System.Windows.Forms.PictureBox cardPictureBox1;
+        private System.Windows.Forms.CheckBox isReConCehckBox;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.TextBox programTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown moveTimeNumericUpDown;
+        private System.Windows.Forms.NumericUpDown twistTimeNumericUpDown;
+    }
+}
---
+++ ProroboQR/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProroboQR
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void programTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //もし、文字末尾が\nだったら、反映する。
+            if (programTextBox.Text.Length > 0)
+            {
+                if (programTextBox.Text.Substring(programTextBox.Text.Length - 1,1) == "\n")
+                {
+                    var commmandsList = new List<string>();
+                    commmandsList.AddRange(programTextBox.Text.Split('\n'));
+
+                }
+            }
+            else
+            {
+                //初期化処理
+            }
+        }
+
+        private void Initial()
+        {
+            programTextBox.Text = "M = " + moveTimeNumericUpDown.Value + "\nT = " + twistTimeNumericUpDown.Value + "\n";
+            programTextBox.Text += "START_UNSAFE\n";
+        }
+
+        private void ChangeCards(List<string> commandsList)
+        {
+            var i = 0;
+            foreach(string s in commandsList)
+            {
+                if (s == "START_UNSAFE")
+                {
+                   
+                }
+            }
+        }
+    }
+}
